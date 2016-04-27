d434a14dbd7111e26862af090202ae3961171c37
kyoo32 <lkwon0302@gmail.com>
2016-04-27 21:53:39 +0900

    PostDeskImageOk

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express =require('express');
 var session = require('express-session');
-
+var formidable = require('formidable');
 var app = express();
 
 app.use(session({
@@ -11,9 +11,9 @@ app.use(session({
 
 var bodyparser = require('body-parser').urlencoded({extended : true});
 app.use(bodyparser);
+
 app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/upload', express.static(__dirname + '/upload'));
-
 
 var handlebars = require('express-handlebars').create({defaultlayout:'main'});
 app.engine('handlebars', handlebars.engine);
@@ -57,6 +57,57 @@ app.get('/logout', function(req, res){
   req.session.destroy();
   res.redirect('/');
 });
+
+app.get('/postNewDesk', function(req, res){
+  res.render('postNewDesk');
+});
+
+app.post('/upload_do', function(req, res){
+  var form = new formidable.IncomingForm();
+  form.uploadDir = __dirname + '/upload';
+  form.keepExtenstions = true;
+
+  form.parse(req, function(err, fields, files){
+    console.log(files + files );
+  });
+  form.on('fileBegin', function(name, file) {
+
+      file.path = __dirname + '/upload/' + file.name;
+   });
+
+   form.on('progress', function(bytesReceived, bytesExpected) {
+     console.log(form.bytesReceived);
+    });
+
+   form.on('end', function(){
+     res.redirect(303, '/upload_ok');
+   });
+
+});
+
+
+app.get('/upload_ok', function(req, res){
+  res.send('loginOk');
+});
+
+app.get('/upload_not', function(req, res){
+  res.send('loginNot');
+});
+
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status('404');
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status('500');
+  res.send('500 - Server Error');
+});
+
+
 app.listen(app.get('port'), function(){
   console.log('Express started on http://locathost' + app.get('port') + ': press Ctrl + C to terminate');
 });
